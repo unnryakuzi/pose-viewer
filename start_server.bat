91e81d8ab1612a22b158f25ac0d239854bc13b2e
unnryakuzi <aken0424@yahoo.co.jp>
2026-01-14 22:42:44 +0900

    改造前

---
+++ start_server.bat
@@ -0,0 +1,23 @@
+@echo off
+cd /d "%~dp0"
+
+set PORT=8000
+set URL=http://localhost:%PORT%
+
+echo.
+echo ==========================================
+echo  Local Unity WebGL Server Started
+echo.
+echo  Open this in your browser:
+echo      %URL%
+echo.
+echo  Press Ctrl+C to stop the server.
+echo ==========================================
+echo.
+
+REM ブラウザを自動で開く（不要ならこの行は削除）
+start "" "%URL%"
+
+python -m http.server %PORT%
+
+pause
